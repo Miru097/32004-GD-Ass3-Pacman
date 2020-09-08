2f651d18f70c0192e584f6991d8b212090634f68
Shuang Zhang <13384983@student.uts.edu.au>
2020-09-08 23:21:35 +1000

    map code modify

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    public int i;
+    private int i;
     public static int Row = 15;
     public static int Column = 14;
     public static int x = -14;
@@ -32,11 +32,11 @@ public class LevelGenerator : MonoBehaviour
     private GameObject[] upperRight = new GameObject[211];
     private GameObject[] lowerLeft = new GameObject[210];
     private GameObject[] lowerRight = new GameObject[210];
-    
+
 
     private static int[] RotationLine = {
         15,28,29,42,43,45,48,50,54,56,57,71,85,99,106,107,113,120,121,126,134,140,146,148,160,162,163,174,176,177,193,207};
-    private static int[] RotationA = { 1,31, 36, 87, 92, 95, 149,179 };//dont move in UpperLeft
+    private static int[] RotationA = { 1, 31, 36, 87, 92, 95, 149, 179 };//dont move in UpperLeft
     private static int[] RotationB = { 34, 40, 90, 93, 112, 132, 138 };//nagetive90 in UL
     private static int[] RotationC = { 59, 64, 70, 101, 109, 127, 135, 154, 190 };//positie90 in UL
     private static int[] RotationD = { 62, 68, 104, 152, 188, 191 };//180 in UL
@@ -65,7 +65,7 @@ public class LevelGenerator : MonoBehaviour
     private void UpperLeft()
     {
         i = 1;
-        
+
         for (int row = 0; row < Row; row++)
         {
             for (int column = 0; column < Column; column++)
@@ -74,44 +74,45 @@ public class LevelGenerator : MonoBehaviour
                 i++;
             }
         }
+
         foreach (int i in RotationLine)
         {
-            upperLeft[i].transform.Rotate(0f, 0f, -90.0f);
+            upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
         }
         foreach (int i in RotationB)
         {
-            upperLeft[i].transform.Rotate(0f, 0f, -90.0f);
+            upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, -90.0f);
         }
         foreach (int i in RotationC)
         {
-            upperLeft[i].transform.Rotate(0f, 0f, 90.0f);
+            upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 90.0f);
         }
         foreach (int i in RotationD)
         {
-            upperLeft[i].transform.Rotate(0, 0, 180.0f);
+            upperLeft[i].transform.rotation = Quaternion.Euler(0f, 0f, 180.0f);
         }
+
     }
     private void UpperRight()
     {
         i = 1;
         x = 13;
         y = 15;
-
-
-
         for (int row = 0; row < Row; row++)
         {
             for (int column = 0; column < Column; column++)
             {
-                upperRight[i] = Instantiate(gameObjects[levelMap[row, column]], new Vector2(x - column, y - row), Quaternion.identity);
-                if (i<=210) i++;
+                upperRight[i] = Instantiate(upperLeft[i], new Vector2(x - column, y - row), Quaternion.identity);
+                //upperRight[i].GetComponent<SpriteRenderer>().flipX = true;
+                if (i <= 210) i++;
             }
         }
-        upperRight[Tjuc].GetComponent<SpriteRenderer>().flipX = true;
+
         foreach (int i in RotationLine)
         {
             upperRight[i].transform.Rotate(0f, 0f, -90.0f);
         }
+
         foreach (int i in RotationA)
         {
             upperRight[i].transform.Rotate(0f, 0f, -90.0f);
@@ -120,10 +121,11 @@ public class LevelGenerator : MonoBehaviour
         {
             upperRight[i].transform.Rotate(0f, 0f, 180.0f);
         }
-        foreach(int i in RotationD)
+        foreach (int i in RotationD)
         {
             upperRight[i].transform.Rotate(0f, 0f, 90f);
         }
+        upperRight[Tjuc].GetComponent<SpriteRenderer>().flipX = true;
 
     }
     private void LowerLeft()
@@ -131,73 +133,72 @@ public class LevelGenerator : MonoBehaviour
         i = 1;
         x = -14;
         y = -13;
-        for (int row = 0; row < Row-1; row++)
+        for (int row = 0; row < Row - 1; row++)
         {
             for (int column = 0; column < Column; column++)
             {
-                lowerLeft[i] = Instantiate(gameObjects[levelMap[row, column]], new Vector2(x+column, y+row), Quaternion.identity);
+                lowerLeft[i] = Instantiate(gameObjects[levelMap[row, column]], new Vector2(x + column, y + row), Quaternion.identity);
                 if (i < lowerLeft.Length) i++;
             }
         }
         lowerLeft[Tjuc].GetComponent<SpriteRenderer>().flipY = true;
         lowerLeft[182] = Instantiate(gameObjects[4], new Vector3(-1, -1), Quaternion.identity);
-        
+
         foreach (int i in RotationLine)
         {
-            if(i<=196) lowerLeft[i].transform.Rotate(0f, 0f, -90.0f);
+            if (i <= 196) lowerLeft[i].transform.Rotate(0f, 0f, -90.0f);
         }
-        
+
         foreach (int i in RotationA)
         {
             lowerLeft[i].transform.Rotate(0f, 0f, 90.0f);
         }
-        
+
         foreach (int i in RotationB)
         {
             lowerLeft[i].transform.Rotate(0f, 0f, 180.0f);
         }
 
-        foreach(int i in RotationD)
+        foreach (int i in RotationD)
         {
             lowerLeft[i].transform.Rotate(0f, 0f, -90f);
         }
-        
     }
     private void LowerRight()
     {
         i = 1;
         x = 13;
         y = -13;
-        for (int row = 0; row < Row-1; row++)
+        for (int row = 0; row < Row - 1; row++)
         {
             for (int column = 0; column < Column; column++)
             {
-                lowerRight[i] = Instantiate(gameObjects[levelMap[row, column]], new Vector2(x-column, y+row), Quaternion.identity);
+                lowerRight[i] = Instantiate(gameObjects[levelMap[row, column]], new Vector2(x - column, y + row), Quaternion.identity);
                 if (i <= 210) i++;
             }
         }
+
         lowerRight[Tjuc].GetComponent<SpriteRenderer>().flipX = true;
         lowerRight[Tjuc].GetComponent<SpriteRenderer>().flipY = true;
         lowerRight[182] = Instantiate(gameObjects[4], new Vector3(0, -1), Quaternion.identity);
 
         foreach (int i in RotationLine)
         {
-            if(i<=196) lowerRight[i].transform.Rotate(0f, 0f, -90.0f);
+            if (i <= 196) lowerRight[i].transform.Rotate(0f, 0f, -90.0f);
         }
-        
+
         foreach (int i in RotationA)
         {
             lowerRight[i].transform.Rotate(0f, 0f, 180.0f);
         }
-        
+
         foreach (int i in RotationC)
         {
             lowerRight[i].transform.Rotate(0f, 0f, -90.0f);
         }
-        foreach(int i in RotationB)
+        foreach (int i in RotationB)
         {
             lowerRight[i].transform.Rotate(0f, 0f, 90f);
         }
-
     }
 }
