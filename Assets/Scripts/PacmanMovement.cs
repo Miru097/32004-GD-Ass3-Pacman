796807c50861d766c5584d3528226bfc0edd751e
Shuang Zhang <13384983@student.uts.edu.au>
2020-09-11 00:16:24 +1000

    pacman basic movement

--- Assets/Scripts/PacmanMovement.cs
+++ Assets/Scripts/PacmanMovement.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PacmanMovement : MonoBehaviour
 {
     private bool switchedOn = false;
+    private GameObject pacman = default;
+    private float timer = default;
+    private Animator pacmanAnimator = default;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        pacman = GameObject.FindWithTag("Pacman");
+        pacman.transform.position = new Vector2(-13, 14);
+        pacmanAnimator = pacman.GetComponent<Animator>();
+        pacmanAnimator.SetTrigger("Pacman_TurnRight");
     }
 
     // Update is called once per frame
@@ -18,6 +25,37 @@ public class PacmanMovement : MonoBehaviour
         {
             GameObject.FindWithTag("SE_notEat").GetComponent<AudioSource>().Play();
             switchedOn = true;
+            timer = 0;
         }
+        if (GameObject.FindWithTag("SE_notEat").GetComponent<AudioSource>().isPlaying == true && timer >= 0.4f)
+        {
+            if(pacman.transform.position.x!=-2 && pacman.transform.position.y == 14)
+            {
+                pacman.transform.Translate(Vector2.right);
+            }
+
+            else if (pacman.transform.position.x == -2 && pacman.transform.position.y != 10)
+            {
+                pacman.transform.Translate(Vector2.down);
+            }
+            else if (pacman.transform.position.x != -13 && pacman.transform.position.y == 10)
+            {
+                pacman.transform.Translate(Vector2.left);
+            }
+            else
+            {
+                pacman.transform.Translate(Vector2.up);
+            }
+            if (pacman.transform.position.x == -13 && pacman.transform.position.y == 14)
+            {
+                pacmanAnimator.SetTrigger("Pacman_TurnRight");
+            }
+            if(pacman.transform.position.x == -2 && pacman.transform.position.y == 10)
+            {
+                pacmanAnimator.SetTrigger("Pacman_TurnLeft");
+            }
+            timer = 0;
+        }
+        timer += Time.deltaTime;
     }
 }
