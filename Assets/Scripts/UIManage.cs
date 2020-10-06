0dda76dcb9792f2c7bf1f54d3220520629a9a37a
Shuang Zhang <13384983@student.uts.edu.au>
2020-10-06 15:00:17 +1100

    50%

---
+++ Assets/Scripts/UIManage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadPacmanLevel()
+    {
+        DontDestroyOnLoad(this);
+        SceneManager.LoadScene(0);
+    }
+    public void LoadDesignLevel()
+    {
+        DontDestroyOnLoad(this);
+        SceneManager.LoadScene(2);
+    }
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+    }
+    }
